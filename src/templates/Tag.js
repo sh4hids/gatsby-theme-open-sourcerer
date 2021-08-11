7d408c4105c76d1336f98616909cf5bc8f4d4319
sh4hids <dev@shahid.pro>
2021-08-11 19:49:35 +0600

    ➕ add tag page

--- src/templates/Tag.js
+++ src/templates/Tag.js
@@ -1,18 +1,29 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { graphql, Link } from 'gatsby';
+import { graphql } from 'gatsby';
 
-import toTitleCase from '../utils/toTitleCase';
 import toKebabCase from '../utils/toKebabCase';
-import { Text } from '../components';
+import toTitleCase from '../utils/toTitleCase';
+import { Text, PostSummaryCard } from '../components';
 import DefaultLayout from '../layouts/DefaultLayout';
 
-const Tag = (props) => {
-  console.log(props);
+const Tag = ({ data, pageContext }) => {
+  const posts = data.allMarkdownRemark.edges;
+  const { blogPath } = data.site.siteMetadata;
 
   return (
-    <DefaultLayout>
-      <Text>No tag to show!</Text>
+    <DefaultLayout
+      title={toTitleCase(pageContext.tag)}
+      url={`/${blogPath}/tags/${toKebabCase(pageContext.tag)}/`}
+      description={`Following are the latest posts related to the topic ${pageContext.tag.toUpperCase()}.`}
+    >
+      {posts.length ? (
+        posts.map((post) => (
+          <PostSummaryCard post={post} blogPath={blogPath} key={post.node.id} />
+        ))
+      ) : (
+        <Text>No post to show!</Text>
+      )}
     </DefaultLayout>
   );
 };
@@ -29,6 +40,7 @@ export const pageQuery = graphql`
       totalCount
       edges {
         node {
+          id
           fields {
             slug
           }
@@ -37,8 +49,14 @@ export const pageQuery = graphql`
           frontmatter {
             title
             tags
+            publishedAt
           }
         }
+      }
+    }
+    site {
+      siteMetadata {
+        blogPath
       }
     }
   }
